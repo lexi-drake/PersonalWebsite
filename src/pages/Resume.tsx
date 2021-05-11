9bba17d639501af6eb925496fa968e9a8a9973fc
alexa <alexa11drake@gmail.com>
2021-05-10 22:20:23 -0500

    redesign

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,27 +1,17 @@
 import Container from "../components/Container";
 import EducationHistory from "../components/EducationHistory";
 import Bio from "../components/Bio";
-import Title from '../components/Title';
 import RelevantLinks from '../components/RelevantLinks';
 import EmploymentHistory from "../components/EmploymentHistory";
 
-interface ResumeProps {
 
-}
-
-const Resume = (props: ResumeProps) => {
-
-    return (
-        <Container>
-            <Title>
-                <h1>Alexa Drake</h1>
-            </Title>
-            <Bio />
-            <EmploymentHistory />
-            <EducationHistory />
-            <RelevantLinks />
-        </Container>
-    )
-}
+const Resume = (props: any) =>
+    <Container>
+        <h1 className="header">Alexa Drake</h1>
+        <Bio />
+        <EmploymentHistory />
+        <EducationHistory />
+        <RelevantLinks />
+    </Container>;
 
 export default Resume;
